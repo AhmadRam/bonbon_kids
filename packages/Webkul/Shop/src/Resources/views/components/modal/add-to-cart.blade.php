78f5eb4a1da4b04be998bbfd4c5de1069945b881
EngAhmadRam <a.n.s.r.96@gmail.com>
2026-08-10 00:18:05 +0300

    Further enlarge payment icons in modal

--- packages/Webkul/Shop/src/Resources/views/components/modal/add-to-cart.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/modal/add-to-cart.blade.php
@@ -63,9 +63,9 @@
                                     <p class="text-sm font-semibold text-toyBlue mb-3">طرق الدفع</p>
                                     <div class="flex justify-center items-center gap-5">
                                         <img src="{{ bagisto_asset('images/cash-on-delivery.png') }}" alt="Cash on Delivery" class="h-8 object-contain">
-                                        <img src="{{ bagisto_asset('images/apple-pay.png') }}" alt="Apple Pay" class="h-8 object-contain" style="transform: scale(1.5); margin: 0 5px;">
-                                        <img src="{{ bagisto_asset('images/cc.png') }}" alt="Visa Mastercard" class="h-8 object-contain" style="transform: scale(1.5); margin: 0 5px;">
-                                        <img src="{{ bagisto_asset('images/knet.png') }}" alt="KNET" class="h-8 object-contain" style="transform: scale(1.5); margin: 0 5px;">
+                                        <img src="{{ bagisto_asset('images/apple-pay.png') }}" alt="Apple Pay" class="h-8 object-contain" style="transform: scale(1.8); margin: 0 7px;">
+                                        <img src="{{ bagisto_asset('images/cc.png') }}" alt="Visa Mastercard" class="h-8 object-contain" style="transform: scale(1.8); margin: 0 7px;">
+                                        <img src="{{ bagisto_asset('images/knet.png') }}" alt="KNET" class="h-8 object-contain" style="transform: scale(1.8); margin: 0 7px;">
                                     </div>
                                 </div>
 
